8226af03666166908fc89e9faa22219172b0161d
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2020-04-13 10:19:44 +0430

    re worked the way writing out text

--- mainScript.py
+++ mainScript.py
@@ -5,6 +5,7 @@ if os.name == 'nt':
     from colorama import init
     init()
 
+
 def showLogo():
     logo = '''                                                                
 ,-.----.                                                        
@@ -24,21 +25,37 @@ def showLogo():
                                                                 '''
     print(logo)
 
+
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def write(word, message, color='green'):
+
+def write(word):
     for i in range(len(word)):
         print(word[i], sep='', end='', flush=True)
         sleep(0.05)
-    
+    sleep(0.09)
+
+def writeC(word, color):
     if color == 'green':
-        print(f'{bcolors.OK}{message}{bcolors.END}', end='')
-    elif color == 'warning':
-        print(f'{bcolors.WARNING}{message}{bcolors.END}', end='')
-    
+        for i in range(len(word)):
+            print(f'{bcolors.OK}{word[i]}{bcolors.END}', sep='', end='', flush=True)
+            sleep(0.05)
+    elif color == 'red':
+        for i in range(len(word)):
+            print(f'{bcolors.WARNING}{word[i]}{bcolors.END}', sep='', end='', flush=True)
+            sleep(0.05)
+    elif color == 'blue':
+        for i in range(len(word)):
+            print(f'{bcolors.BLUE}{word[i]}{bcolors.END}', sep='', end='', flush=True)
+            sleep(0.05)
+    else:
+        write(message)
+
+
 def initSystem():
-    write("Booting System...", "OK", "green")
+    write("Booting System...")
+    writeC("OK", "green")
 
 cls()
 showLogo()
