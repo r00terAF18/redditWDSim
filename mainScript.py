a5078aaf7111e1ad06e898d4698dd554d7bae9a9
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2020-04-13 09:38:19 +0430

    got the hacker style output working, colors need work

--- mainScript.py
+++ mainScript.py
@@ -1,5 +1,7 @@
 import bcolors
 import os
+from time import sleep
+from termcolor import colored
 
 def showLogo():
     logo = '''                                                                
@@ -23,10 +25,22 @@ def showLogo():
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def initSystem():
+def write(word, message, color='green'):
+    for i in range(len(word)):
+        print(word[i], sep='', end='', flush=True)
+        sleep(0.05)
     
-
+    if color == 'green':
+        print(colored(message, color))
+        print(f'{bcolors.OK}{message}{bcolors.END}', end='')
+    elif color == 'warning':
+        print(f'{bcolors.WARNING}{message}{bcolors.END}', end='')
+    
+def initSystem():
+    write("Booting System...", "OK", "green")
 
 cls()
 showLogo()
+
+initSystem()
 
